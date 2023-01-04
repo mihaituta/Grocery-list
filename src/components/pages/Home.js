857e6d67db46d06aefc46d7b6060b4a0120eba35
mihaituta <tutamihai@gmail.com>
2023-01-04 04:28:33 +0200

    added static list

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { UserAuth } from '../Auth/AuthContext'
 
+const items = ['Cola', 'Potato Chips', 'Pudding', 'Chocolate']
+
 const Account = () => {
   const { user, logout } = UserAuth()
   const navigate = useNavigate()
@@ -16,6 +18,29 @@ const Account = () => {
     }
   }
 
+  const foodItems = (
+    <ul className='mt-2'>
+      {items.map((item, index) => (
+        <li key={index}>
+          <label className='inline-flex items-center bg-zinc-500	 mt-2 p-2 w-full'>
+            <input
+              className='w-8 h-8 text-green-600 border-0 focus:ring-0 rounded-full'
+              type='checkbox'
+              id={`checkbox-${index}`}
+              name={item}
+            />
+            <label
+              className='ml-3 text-2xl font-semibold text-white'
+              htmlFor={`checkbox-${index}`}
+            >
+              {item}
+            </label>
+          </label>
+        </li>
+      ))}
+    </ul>
+  )
+
   return (
     <div className='max-w-[600px] mx-auto my-16 p-4'>
       <h1 className='text-2xl font-bold py-4'>Account</h1>
@@ -24,6 +49,7 @@ const Account = () => {
       <button onClick={handleLogout} className='border px-6 py-2 my-4'>
         Logout
       </button>
+      {foodItems}
     </div>
   )
 }
--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -6,8 +6,8 @@ const Register = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
-  const { createUser } = UserAuth()
   const navigate = useNavigate()
+  const { createUser } = UserAuth()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
