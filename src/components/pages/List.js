8e0173a2446c2469c7ca74ed5c15dd451e643272
mihaituta <tutamihai@gmail.com>
2023-01-13 15:25:28 +0200

    added yellow color to some items

--- src/components/pages/List.js
+++ src/components/pages/List.js
@@ -2,6 +2,7 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { useContext, useEffect } from 'react'
 import { ListsContext } from '../../store/ListsContextProvider'
 import { UserAuth } from '../auth/AuthContext'
+import { PlusIcon } from '@heroicons/react/24/solid'
 
 const List = () => {
   const { user } = UserAuth()
@@ -48,10 +49,10 @@ const List = () => {
       {listsCtx.currentList.foodItems &&
         listsCtx.currentList.foodItems.map((foodItem, index) => (
           <li key={index}>
-            <label className='flex items-center bg-zinc-800 mt-2 px-4 py-3 mx-2 rounded '>
+            <label className='flex items-center bg-zinc-800 mt-2 px-4 py-3 mx-2 rounded'>
               <input
-                className='w-6 h-6 text-sky-600  bg-zinc-800
-                focus:ring-offset-0 border-2 border-white focus:ring-0 ring-0 rounded-full '
+                className='w-7 h-7  text-yellow-500     bg-zinc-800
+                focus:ring-offset-0 border-2 border-amber-300 focus:ring-0 ring-0 rounded-full '
                 type='checkbox'
                 id={`checkbox-${index}`}
                 name={foodItem.name}
@@ -73,18 +74,38 @@ const List = () => {
     <>
       <div className='bg-neutral-900 min-h-screen min-h-full'>
         <div className='bg-zinc-800 p-4'>
-          <h1 className='dark:text-white text-3xl font-semibold py-2'>
-            Grocery list
-          </h1>
+          <div className='flex flex-row items-center justify-between '>
+            <h1 className='dark:text-white text-3xl font-semibold py-2'>
+              Grocery list
+            </h1>
+            <button
+              onClick={deleteList}
+              className='border px-6 h-11 text-lg  text-amber-300 border-amber-300'
+            >
+              Delete list
+            </button>
+          </div>
           <p className='dark:text-white text-base mt-2 mb-4'>
             {user && user.email}
           </p>
-          <button
-            className='border px-6 h-11 text-xl dark:text-white'
-            onClick={addFood}
-          >
-            Add Food
-          </button>
+          <div className='flex items-center'>
+            <input
+              type='text'
+              // placeholder='Enter item here...'
+              placeholder='Add new item...'
+              className='bg-neutral-900 text-white text-lg placeholder-neutral-600
+             border-0 focus:ring-0 ring-0 rounded w-48'
+            />
+
+            <button
+              className='border w-10 h-10 ml-2 text-xl dark:text-white border-amber-300
+              rounded flex items-center justify-center'
+              onClick={addFood}
+            >
+              <PlusIcon className='w-5 h-5 text-amber-300' />
+            </button>
+          </div>
+
           {/*<button
           className='dark:text-white border px-6 py-2  m-4'
           onClick={show}
